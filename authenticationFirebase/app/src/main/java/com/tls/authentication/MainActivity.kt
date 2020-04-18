646dec93c66bef27e8909b26c97ea013059578c2
Thiago Lopes da Silva <tls@cesar.org.br>
2020-04-18 14:50:48 -0300

    [authenticationFirebase] fixed toolbar title and app theme

--- authenticationFirebase/app/src/main/java/com/tls/authentication/MainActivity.kt
+++ authenticationFirebase/app/src/main/java/com/tls/authentication/MainActivity.kt
@@ -15,7 +15,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
     }
 
 }
